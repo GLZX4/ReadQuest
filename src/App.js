a419a4fc10ef2f19c8bf5874046153902bd4198d
GEORGE HARRISON <2218541@chester.ac.uk>
2024-09-24 14:33:35 +0100

    Initial Push

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to ReadQuest!</h1>
+      <p>This is the beginning of your gamified reading comprehension platform.</p>
+    </div>
+  );
+}
+
+export default App;
